65be8404fa7943da3d5bb775f5c6d059a4e7b06b
CHARITY-WACHIRA <kouwywachira@gmail.com>
2024-06-09 11:50:53 +0300

    completed assignment

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,20 @@
 class Tree:
+
   def __init__(self, root = None):
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    """Traverse the tree using depth-first search and return the node with the given id.
+    If no node is found with the given id, return None."""
+    stack =[self.root]
+
+    while stack:
+      node=stack.pop()
+
+      if node.get('id')==id:
+        return node
+      
+      stack.extend (reversed(node.get('children',[])))
+
+    return None
+
